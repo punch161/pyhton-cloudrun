ed2a467a09961286c11b0d6c766587b8b2737995
punch161 <muhammadhamza@punch.cool>
2023-01-20 15:25:24 +0500

    host added settings.py

--- src/app/settings.py
+++ src/app/settings.py
@@ -94,7 +94,8 @@ DATABASES = {"default": env.db()}
 
 # If the flag as been set, configure to use proxy
 if os.getenv("USE_CLOUD_SQL_AUTH_PROXY", None):
-    DATABASES["default"]["HOST"] = "cloudsql-proxy"
+    # DATABASES["default"]["HOST"] = "cloudsql-proxy"
+    DATABASES["default"]["HOST"] = "127.0.0.1"
     DATABASES["default"]["PORT"] = 5432
 
 # Password validation
